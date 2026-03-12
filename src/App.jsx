dda0b1248c976cb2f1a2bcf5e82ca1b330b9bf19
nick <11andriianovnikita@gmail.com>
2026-03-13 01:25:43 +0300

    vdvd

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on аиавиаиа  and React \ аики to fkjdfklbvd
+        Click on вывсмвм  and React \ аики to fkjdfklbvd
       </p>
     </>
   )
